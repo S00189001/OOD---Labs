c56daa54f6b84f6e34563ace1188d28bb9a015f0
Matthew Bowen <S00189001@mail.itsligo.ie>
2020-01-22 13:55:29 +0000

    Added IComparable

---
+++ Week1Lab1/Week1Lab1/Bands.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Lab1
+{
+    public enum BandGenre
+    {
+        Rock,
+        Pop,
+        Indie
+    }
+
+    public abstract class Bands : IComparable
+    {
+        public BandGenre genre { get; set; }
+        public string bandName { get; set; }
+        public DateTime yearFormed { get; set; }
+        public string members { get; set; }
+
+        //List<string> members = new List<string>();
+        //List<string> albums = new List<string>();
+
+        public Bands()
+        {
+        }
+
+        public Bands(string genre, string bandName, DateTime yearFormed)
+        {
+            genre = this.genre.ToString();
+            bandName = this.bandName.ToString();
+            yearFormed = this.yearFormed;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{bandName}  -  {genre}");
+        }
+
+        public int CompareTo(object obj)
+        {
+            Bands BandObject = obj as Bands;
+            return this.bandName.CompareTo(BandObject.bandName);
+
+        }
+
+
+
+        //public Bands(string genre, string bandName, int yearFormed)
+        //{
+        //    genre = this.genre;
+        //    bandName = this.bandName;
+        //    yearFormed = this.yearFormed;
+        //}
+
+    }
+
+    public class Rock : Bands
+    {
+        public Rock()
+        {
+            genre = BandGenre.Rock;
+        }
+    }
+
+    public class Pop : Bands
+    {
+        public Pop()
+        {
+            genre = BandGenre.Pop;
+        }
+    }
+
+    public class Indie : Bands
+    {
+        public Indie()
+        {
+            genre = BandGenre.Indie;
+        }
+    }
+}
---
+++ Week1Lab1/Week1Lab1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week1Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Bands> listOfBands = new List<Bands>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            listOfBands.Add(new Rock()
+            {
+                bandName = "AC / DC",
+                yearFormed = new DateTime(1973, 1, 1),
+                members = "Angus Young, Brian Johnson, Bon Scott"
+            });
+            listOfBands.Add(new Rock()
+            {  
+                bandName = "The Beatles",
+                yearFormed = new DateTime(1960,1,1),
+                members = "John Lennon, Paul McCartney, Ringo Starr"
+
+            });
+            listOfBands.Add(new Pop()
+            {
+                bandName = "The Spice Boys",
+                yearFormed = new DateTime(2020,1,1),
+                members = "Adil, Vitor, Mohamed"
+
+            });
+            listOfBands.Add(new Pop()
+            {
+                bandName = "The Bee Gees",
+                yearFormed = new DateTime(1958,1,1),
+                members = " Barry Gibb, Robin Gibb, Maurice Gibb, Don Henley"
+
+            });
+            listOfBands.Add(new Indie()
+            {
+                bandName = "Arctic Monkeys",
+                yearFormed = new DateTime(2000,1,1),
+                members = "Alex Turner, Matt Helders, Jamie Cook, Nick O'Malley, Andy Nicholson, Glyn Jones"
+
+            });
+            listOfBands.Add(new Indie()
+            {
+                bandName = "The Strokes",
+                yearFormed = new DateTime(1998,1,1),
+                members = "Julian Casablancas, Albert Hammond Jr., Nick Valensi, Fabrizio Moretti, Nikolai Fraiture"
+
+            });
+
+            UI_BandsListbox.ItemsSource = listOfBands;
+            listOfBands.Sort();
+            
+        }
+
+        private void UI_BandsListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
